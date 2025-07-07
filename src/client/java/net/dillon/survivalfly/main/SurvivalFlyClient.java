94a06151fe12d3b8d4460b02205895d66f3e82d8
Dillon <dilloneystrickland12@gmail.com>
2025-07-07 14:53:17 -0400

    looking good for 1.21.7!

--- src/client/java/net/dillon/survivalfly/main/SurvivalFlyClient.java
+++ src/client/java/net/dillon/survivalfly/main/SurvivalFlyClient.java
@@ -2,13 +2,8 @@ package net.dillon.survivalfly.main;
 
 import net.dillon.survivalfly.keybind.ModKeybinds;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 
 public class SurvivalFlyClient implements ClientModInitializer {
-	private static final String MOD_KEYBINDS = "survivalfly.title.options";
-	public static final KeyBinding OPEN_CONFIG_SCREEN = KeyBindingHelper.registerKeyBinding(new KeyBinding("survivalfly.options.open_config_screen", InputUtil.GLFW_KEY_F, MOD_KEYBINDS));
 
 	@Override
 	public void onInitializeClient() {
