6385493503c5afb092c8ee322cc4d9a6acb413d7
Dillon <dilloneystrickland12@gmail.com>
2026-09-20 13:50:49 -0400

    update to dillonlib 1.2

--- common/src/main/java/net/dillon/survivalfly/screen/MainMenuScreen.java
+++ common/src/main/java/net/dillon/survivalfly/screen/MainMenuScreen.java
@@ -2,6 +2,7 @@ package net.dillon.survivalfly.screen;
 
 import net.dillon.dillonlib.annotation.Dill;
 import net.dillon.dillonlib.annotation.DillType;
+import net.dillon.dillonlib.screen.DillonLibScreen;
 import net.dillon.dillonlib.task.ClientTasks;
 import net.dillon.dillonlib.util.KeybindScrollHelper;
 import net.dillon.dillonlib.util.Links;
@@ -13,12 +14,10 @@ import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.options.OptionsSubScreen;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsScreen;
 import net.minecraft.network.chat.Component;
 
 import java.net.URI;
-import java.util.List;
 
 import static net.dillon.dillonlib.task.ClientTasks.openDebugEntriesScreen;
 import static net.dillon.dillonlib.task.ClientTasks.openScreen;
@@ -26,53 +25,17 @@ import static net.dillon.survivalfly.helper.ModConstants.HAS_UPDATE;
 import static net.dillon.survivalfly.helper.ModConstants.VERSION;
 
 /**
- * The options screen for configurating the {@code /fly} command.
+ * The options screen for configurating the mod in a singleplayer world.
  */
 @Dill(DillType.CLIENT)
-public class MainMenuScreen extends OptionsSubScreen {
+public class MainMenuScreen extends DillonLibScreen {
 
     public MainMenuScreen(Screen parent) {
-        super(parent, Minecraft.getInstance().options, Component.translatable("survivalfly.menu.title"));
+        super(parent, Component.translatable("survivalfly.menu.title"));
     }
 
     @Override
-    protected void init() {
-        super.init();
-
-        this.list.addHeader(Component.translatable("survivalfly.menu.settings"));
-        this.list.addSmall(
-                List.of(
-                        Button.builder(Component.translatable("survivalfly.menu.configure"), button -> ClientTasks.tryOpenYaclScreen(
-                                () -> ConfigurationScreen.configScreen().generateScreen(this),
-                                Component.translatable("survivalfly.title")
-                        )).build(),
-
-                        Button.builder(Component.translatable("survivalfly.menu.keybinds"), button -> {
-                            KeybindScrollHelper.request(ModKeyMappings.SURVIVAL_FLY);
-                            openScreen(new KeyBindsScreen(this, Minecraft.getInstance().options));
-                        }).build()
-                )
-        );
-
-        this.list.addHeader(Component.translatable("survivalfly.menu.resources_and_utilities"));
-        this.list.addSmall(
-                List.of(
-                        Button.builder(Component.translatable("survivalfly.gui.ask_questions"), ConfirmLinkScreen.confirmLink(this, URI.create(Links.DILLONS_DISCORD), false))
-                                .build(),
-
-                        Button.builder(Component.translatable("survivalfly.menu.debug_huds"), button -> openDebugEntriesScreen(this, "survivalfly"))
-                                .build(),
-
-                        Button.builder(Component.translatable("survivalfly.gui.report_bugs"), ConfirmLinkScreen.confirmLink(this, URI.create(Links.githubIssues("Dillon8775/Survival-Fly")), false))
-                                .build()
-                )
-        );
-    }
-
-    @Override
-    public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float deltaTicks) {
-        super.extractRenderState(graphics, mouseX, mouseY, deltaTicks);
-
+    protected void renderModInfo(GuiGraphicsExtractor graphics) {
         ClientTasks.drawModInfo(
                 graphics,
                 this,
@@ -83,6 +46,30 @@ public class MainMenuScreen extends OptionsSubScreen {
     }
 
     @Override
-    protected void addOptions() {
+    protected void widgets() {
+        this.createHeader(
+                Component.translatable("survivalfly.menu.settings"),
+                Button.builder(Component.translatable("survivalfly.menu.configure"), button -> ClientTasks.tryOpenYaclScreen(
+                        () -> ConfigurationScreen.configScreen().generateScreen(this),
+                        Component.translatable("survivalfly.title")
+                )).build(),
+
+                Button.builder(Component.translatable("survivalfly.menu.keybinds"), button -> {
+                    KeybindScrollHelper.request(ModKeyMappings.SURVIVAL_FLY);
+                    openScreen(new KeyBindsScreen(this, Minecraft.getInstance().options));
+                }).build()
+        );
+
+        this.createHeader(
+                Component.translatable("survivalfly.menu.resources_and_utilities"),
+                Button.builder(Component.translatable("survivalfly.gui.ask_questions"), ConfirmLinkScreen.confirmLink(this, URI.create(Links.DILLONS_DISCORD), false))
+                        .build(),
+
+                Button.builder(Component.translatable("survivalfly.menu.debug_huds"), button -> openDebugEntriesScreen(this, "survivalfly"))
+                        .build(),
+
+                Button.builder(Component.translatable("survivalfly.gui.report_bugs"), ConfirmLinkScreen.confirmLink(this, URI.create(Links.githubIssues("Dillon8775/Survival-Fly")), false))
+                        .build()
+        );
     }
 }
